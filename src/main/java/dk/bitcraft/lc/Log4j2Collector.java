4c024aa710294e219530c871835b09fa919e8e34
Henrik Aasted Sørensen <has@bitcraft.dk>
2017-07-17 20:19:12 +0200

    Rudimentary log4j2 support.

--- src/main/java/dk/bitcraft/lc/Log4j2Collector.java
+++ src/main/java/dk/bitcraft/lc/Log4j2Collector.java
@@ -1,11 +1,19 @@
 package dk.bitcraft.lc;
 
-import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.appender.AbstractAppender;
+import org.apache.logging.log4j.core.layout.PatternLayout;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+
+import static java.util.stream.Collectors.toList;
 
 class Log4j2Collector implements CollectorImpl {
     private final Logger logger;
+    ListAppender appender;
 
     public Log4j2Collector(Object logger) {
         this.logger = (Logger) logger;
@@ -14,16 +22,32 @@ class Log4j2Collector implements CollectorImpl {
 
     @Override
     public void setup() {
+        appender = new ListAppender(UUID.randomUUID().toString());
+        appender.start();
 
+        logger.addAppender(appender);
     }
 
     @Override
     public void remove() {
-
+        logger.removeAppender(appender);
     }
 
     @Override
     public List<String> getResult() {
-        return null;
+        return appender.events.stream().map(e -> e.toString()).collect(toList());
+    }
+
+    class ListAppender extends AbstractAppender {
+        List<LogEvent> events = new ArrayList<>();
+
+        ListAppender(String name) {
+            super(name, null, PatternLayout.createDefaultLayout());
+        }
+
+        @Override
+        public void append(LogEvent event) {
+            events.add(event);
+        }
     }
 }
