5c17a484ec57e385f030c206e78d8cc43f5a4203
Henrik Aasted Sørensen <has@bitcraft.dk>
2017-07-25 16:12:02 +0200

    Use an enumeration to list supported frameworks.

--- src/main/java/dk/bitcraft/lc/LogCollector.java
+++ src/main/java/dk/bitcraft/lc/LogCollector.java
@@ -2,6 +2,7 @@ package dk.bitcraft.lc;
 
 import org.junit.rules.ExternalResource;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,47 +10,13 @@ import java.util.Optional;
 public class LogCollector extends ExternalResource {
     private CollectorImpl collector;
 
-    // TODO Turn into an enum?
-    private static final Optional<Class> log4j2 = lookFor("org.apache.logging.log4j.Logger");
-
-    private static final Optional<Class> Logback = lookFor("ch.qos.logback.classic.Logger");
-
-    private static final Optional<Class> javaUtilLogging = lookFor("java.util.logging.Logger");
-
-    private static final Optional<Class> slf4jLog4j2 = lookFor("org.apache.logging.slf4j.Log4jLogger");
-
-
-
-    private static Optional<Class> lookFor(String s) {
-        try {
-            return Optional.of(Class.forName(s));
-        } catch (ClassNotFoundException e) {
-            return Optional.empty();
-        }
-    }
-
     public LogCollector(Object logger) {
-        if (Logback.filter(c -> c.isInstance(logger)).isPresent()) {
-            collector = new LogBackCollector(logger);
-            return;
-        }
-
-        if (log4j2.filter(c -> c.isInstance(logger)).isPresent()) {
-            collector = new Log4j2Collector(logger);
-            return;
-        }
-
-        if (slf4jLog4j2.filter(c -> c.isInstance(logger)).isPresent()) {
-            collector = SLF4J_Log4j2Collector.create(logger);
-            return;
-        }
-
-        if (javaUtilLogging.filter(c -> c.isInstance(logger)).isPresent()) {
-            collector = new JavaUtilLoggingCollector(logger);
-            return;
-        }
-
-        throw new RuntimeException("Unknown logger " + logger.getClass());
+        collector = Arrays.stream(Frameworks.values())
+                .map(v -> v.getCollector(logger))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Unknown logger " + logger.getClass()));
     }
 
     @Override
