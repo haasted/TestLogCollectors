3db4a470a0902b945b458305841f82eaccc894c4
Henrik Aasted Sørensen <has@bitcraft.dk>
2017-07-26 21:33:10 +0200

    established test environment.

--- src/main/java/dk/bitcraft/lc/Frameworks.java
+++ src/main/java/dk/bitcraft/lc/Frameworks.java
@@ -9,7 +9,8 @@ enum Frameworks {
     Log4j2("org.apache.logging.log4j.Logger", v -> new Log4j2Collector(v)),
     JavaUtilLogging("java.util.logging.Logger", v -> new JavaUtilLoggingCollector(v)),
     Log4j2_slf4j("org.apache.logging.slf4j.Log4jLogger", v -> SLF4J_Log4j2Collector.create(v)),
-    javaUtilLogging_slf4j("org.slf4j.impl.JDK14LoggerAdapter", v -> SLF4J_JavaUtilLoggingCollector.create(v));
+    javaUtilLogging_slf4j("org.slf4j.impl.JDK14LoggerAdapter", v -> SLF4J_JavaUtilLoggingCollector.create(v)),
+    slf4jSimple("org.slf4j.impl.SimpleLogger", v -> SLF4J_JavaUtilLoggingCollector.create(v));
 
     private final Optional<Class> clazz;
     private final Function<Object, CollectorImpl> creator;
---
+++ test-environments/slf4j/src/test/java/test/RuleTest.java
@@ -0,0 +1,32 @@
+package test;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dk.bitcraft.lc.LogCollector;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RuleTest {
+    final static Logger logger = LoggerFactory.getLogger(RuleTest.class);
+
+
+    @Rule
+    public LogCollector collector = new LogCollector(logger);
+
+
+    @Test
+    public void test() {
+        {
+            logger.error("This is an error!");
+            logger.error("This is another error!");
+        }
+
+        assertThat(collector.getLogs())
+                .hasSize(2)
+                .contains("This is an error!", "This is another error!");
+    }
+
+}
