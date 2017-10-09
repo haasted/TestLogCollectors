4360e72905aaa5b9b09118e931921fc2ec5c3b36
Henrik Aasted Sørensen <has@bitcraft.dk>
2017-10-09 21:31:34 +0200

    Format using configured layouts.

--- test-environments/log4j2/src/test/java/test/RuleTest.java
+++ test-environments/log4j2/src/test/java/test/RuleTest.java
@@ -16,27 +16,26 @@ public class RuleTest {
 
     @Test
     public void test() {
-        {
-            Logger logger = LogManager.getLogger("test.logger");
-            logger.error("This is an error!");
-            logger.error("This is another error!");
-        }
+        Logger logger = LogManager.getLogger("test.logger");
+        logger.error("This is an error!");
+        logger.error("This is another error!");
 
-        assertThat(collector.getLogs())
-                .hasSize(2)
-                .contains("This is an error!", "This is another error!");
+
+        assertThat(collector.getLogs()).hasSize(2);
+        assertThat(collector.getLogs().get(0))
+                .contains("This is an error!");
+        assertThat(collector.getLogs().get(1))
+                .contains("This is another error!");
     }
 
 
     @Test
     public void verifyAdditivity() {
-        {
-            Logger logger = LogManager.getLogger("test.logger.subLogger");
-            logger.error("This should be available in the collector");
-        }
+        Logger logger = LogManager.getLogger("test.logger.subLogger");
+        logger.error("This should be available in the collector");
 
-        assertThat(collector.getLogs())
-                .hasSize(1)
+        assertThat(collector.getLogs()).hasSize(1);
+        assertThat(collector.getLogs().get(0))
                 .contains("This should be available in the collector");
     }
 }
